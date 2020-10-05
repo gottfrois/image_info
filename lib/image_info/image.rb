0522d078d112bef91ff9eb61e6af885255486d25
Issei.M <issei.m7@gmail.com>
2020-10-06 00:38:20 +0900

    Stop setting URI port when it doesn't have specified it (#10)
    * Stop setting URI port when it doesn't have specified it
    
    * Fix CS

--- lib/image_info/image.rb
+++ lib/image_info/image.rb
@@ -7,8 +7,7 @@ module ImageInfo
 
     def initialize(uri)
       @uri        = ::Addressable::URI.parse(uri.to_s)
-      @uri.scheme = 'http'  unless @uri.scheme
-      @uri.port   = 80      unless @uri.port
+      @uri.scheme = 'http' unless @uri.scheme
       @uri.normalize!
     rescue ::Addressable::URI::InvalidURIError
       @uri = NullUri.new
